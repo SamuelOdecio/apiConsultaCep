c6fa65dc8a3842868399919b893e37f2d6555de7
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2022-11-18 22:40:37 -0400

    Loading - Consultar Button

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
+import '@picocss/pico'
+
 const formConsultarCep = document.querySelector('#consultarCep')
 const inputCep = formConsultarCep.cep
 const divDados = document.querySelector('#dados')
+const btnConsultarCep = document.querySelector('#btnConsultarCep')
 
 formConsultarCep.addEventListener('submit', (event) => {
   event.preventDefault() //anula o comportamento padrao de envio do form
+  ativaLoader(true)
   consultarCep(inputCep.value)
 
-})
 
+
+})
 
 
 async function consultarCep(cep) {
   let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
   let dadosCep = await response.json()
-
+  
   divDados.innerHTML = `
+  
     <p>Endereço: ${dadosCep.logradouro}</p>
+    <p>Cidade: ${dadosCep.localidade}</p>
+    <p>Estado: ${dadosCep.uf}</p>
     <p>CEP: ${dadosCep.cep}</p>
 
 
   `
-  
+  ativaLoader(false)
+}
+
+function ativaLoader(ativo){
+  if(ativo){
+    btnConsultarCep.setAttribute('aria-busy', 'true')
+    btnConsultarCep.textContent = 'Consultando CEP...'
+
+  }else{
+    btnConsultarCep.removeAttribute('aria-busy')
+    btnConsultarCep.textContent = 'Consultar'
+  }
 }
 
 consultarCep()
